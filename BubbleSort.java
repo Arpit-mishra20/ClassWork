883bf67383affb549223f14c07fe261d4d28b2b8
Arpit-mishra20 <arpit.202033@gmail.com>
2025-02-14 21:39:48 +0530

    Added solution files of Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class BubbleSort{
+
+    public static void bubbleSort(int[] marks){
+        int n=marks.length;
+
+        for(int i=0;i<n-1;i++){
+        boolean isSwapped=false;
+            for(int j=0;j=n-i-1;j++){
+                if(marks[j]>marks[j+1]){
+                    int temp=marks[j];
+                    marks[j]=marks[j+1];
+                    marks[j+1]=temp;
+                    isSwapped=true;
+                }
+            }
+            if(!isSwapped){
+                break;
+            }
+        }
+    }
+
+   public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] studentMarks=new int[n];
+    System.out.println("Enter the marks: ");
+
+    for(int i=0;i<n;i++){
+        studentMarks[i]=sc.nextInt();
+    }
+    bubbleSort(studentMarks);
+    System.out.println("Sorted Marks:");
+    for(int marks:studentMarks){
+        System.out.println(marks+" ");
+    }
+   }
+
+}
